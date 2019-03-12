f554a6721f0cb3fbee3e3960a18e570d992c1af7
ericcchiu <eric.chn.chiu@gmail.com>
2019-03-12 17:49:50 -0500

    (FEATURE) Add Form component

---
+++ client/src/components/Form.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+const Form = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <form>
+      <input
+        value={firstName}
+        onChange={event => setFirstName(event.target.value)}
+        placeholder="First name"
+        type="text"
+        name="firstName"
+        required
+      />
+      <input
+        value={lastName}
+        onChange={event => setLastName(event.target.value)}
+        placeholder="Last name"
+        type="text"
+        name="lastName"
+        required
+      />
+      <input
+        value={email}
+        onChange={event => setEmail(event.target.value)}
+        placeholder="Email address"
+        type="email"
+        name="email"
+        required
+      />
+      <input
+        value={password}
+        onChange={event => setPassword(event.target.value)}
+        placeholder="Password"
+        type="password"
+        name="password"
+        required
+      />
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+export default Form;
