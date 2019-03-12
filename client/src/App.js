4158eded941d16b90ec257a44cb76997a0246bca
ericcchiu <eric.chn.chiu@gmail.com>
2019-03-12 17:51:21 -0500

    (update) Add Form component for rendering

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Form from './components/Form';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Welcome to React Boiler Plate</h1>
+        <Form />
       </div>
     )
   }
