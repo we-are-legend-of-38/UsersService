4bbf75fb70a6e5d189c36120f42cd215d05bfdba
ericcchiu <eric.chn.chiu@gmail.com>
2019-03-13 09:38:31 -0500

    (FEATURE) Update and link new components to main app

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import Form from './components/Form';
 import Users from './components/Users';
+import Dropdown from './components/Dropdown';
+import SearchBar from './components/SearchComponent/SearchComponent';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="user-container">
         <Users />
+        <SearchBar />
+        <Dropdown />
         <Form />
       </div>
     )
