ddb56ba5735006e7aaa22b32744738443267fd81
ericcchiu <eric.chn.chiu@gmail.com>
2019-03-12 21:59:39 -0500

    (SETUP) Set up and connect MongoDB

--- database/MongoDB/mongoDB.js
+++ database/MongoDB/mongoDB.js
@@ -1,18 +1,53 @@
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb://127.0.0.1/Legend38';
+const port = 27017;
+const mongoDB = `mongodb://localhost:${port}/Legend38`;
+const options = {
+  useNewUrlParser: true,
+}
 
-mongoose.connect(mongoDB, { userNewUrlParser: true });
+// Mongoose presets 
 mongoose.Promise = global.Promise;
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+const db = mongoose.connect(mongoDB, { useNewUrlParser: true });
 
-
-let userSchema = mongoose.Schema({
+// Create a User schema
+const userSchema = new mongoose.Schema({
   firstName: { type: String, unique: false },
   lastName: { type: String, unique: false },
   email: { type: String, unique: true },
   password: { type: String, unique: false }
 });
 
-let User = mongoose.model('User', userSchema);
+//  Create a User Model
+const User = mongoose.model('User', userSchema);
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Error connecting to MongoDB'));
+// Create connection 
+const mongoDBConnection = mongoose.connection;
+
+mongoDBConnection.on('connected', () => console.log(`Success: Database connected to port: ${port}`))
+mongoDBConnection.on('error', console.error.bind(console, 'Error connecting to MongoDB'));
+
+
+// Mongoose methods 
+const save = async (userObj) => {
+  const { firstName, lastName, email, password } = await JSON.parse(userObj).singleUser;
+  const userDocument = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    password: password
+  }
+
+  const newUserDoc = new User(userDocument);
+
+  newUserDoc.save()
+    .then(() => console.log('Success in saving documents'))
+    .catch(err => console.log('Error creating new User', err));
+}
+
+
+
+module.exports = {
+  db, User, save
+}
